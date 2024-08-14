b3cd9bb1828ef0fd6850b4d170935c5dac8f0a0d
Sandeep Reddy <60704433+san-deep-reddy@users.noreply.github.com>
2024-08-13 23:42:17 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,57 +1,57 @@
 import os
 
 # API params
-PATH_LAST_PROCESSED = "./data/last_processed.json"
+PATH_LAST_PROCESSED = "./data/last_weather_processed.json"  # Changed to reflect weather data processing
 MAX_LIMIT = 100
 MAX_OFFSET = 10000
 
 # We have three parameters in the URL:
-# 1. MAX_LIMIT: the maximum number of records to be returned by the API
-# 2. date_de_publication: the date from which we want to get the data
-# 3. offset: the index of the first result
-URL_API = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/rappelconso0/records?limit={}&where=date_de_publication%20%3E%20'{}'&order_by=date_de_publication%20ASC&offset={}"
-URL_API = URL_API.format(MAX_LIMIT, "{}", "{}")
+# 1. MAX_LIMIT: the maximum number of records to be returned by the API (not directly applicable to OpenWeather, but kept for consistency)
+# 2. CITY_NAME: the city for which we want to get the weather data
+# 3. API_KEY: the key to authenticate with the OpenWeather API
+CITY_NAME = "London"
+API_KEY = os.getenv("OPENWEATHER_API_KEY", "your_default_api_key")
+URL_API = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+URL_API = URL_API.format(CITY_NAME, API_KEY)
 
 # POSTGRES PARAMS
 user_name = os.getenv("POSTGRES_DOCKER_USER", "localhost")
-POSTGRES_URL = f"jdbc:postgresql://{user_name}:5432/postgres"
+POSTGRES_URL = f"jdbc:postgresql://{user_name}:5432/weather_db"  # Changed the database name to 'weather_db'
 POSTGRES_PROPERTIES = {
     "user": "postgres",
     "password": os.getenv("POSTGRES_PASSWORD"),
     "driver": "org.postgresql.Driver",
 }
 
+# Columns specific to the weather data that might be new
 NEW_COLUMNS = [
-    "risques_pour_le_consommateur",
-    "recommandations_sante",
-    "date_debut_commercialisation",
-    "date_fin_commercialisation",
-    "informations_complementaires",
+    "weather_description",
+    "temperature",
+    "humidity",
+    "pressure",
+    "wind_speed",
 ]
 
+# Columns to normalize from the weather data response
 COLUMNS_TO_NORMALIZE = [
-    "categorie_de_produit",
-    "sous_categorie_de_produit",
-    "nom_de_la_marque_du_produit",
-    "noms_des_modeles_ou_references",
-    "identification_des_produits",
-    "conditionnements",
-    "temperature_de_conservation",
-    "zone_geographique_de_vente",
-    "distributeurs",
-    "motif_du_rappel",
-    "numero_de_contact",
-    "modalites_de_compensation",
+    "weather",
+    "main",
+    "wind",
+    "clouds",
+    "sys",
 ]
 
+# Columns to keep from the weather data response
 COLUMNS_TO_KEEP = [
-    "reference_fiche",
-    "liens_vers_les_images",
-    "lien_vers_la_liste_des_produits",
-    "lien_vers_la_liste_des_distributeurs",
-    "lien_vers_affichette_pdf",
-    "lien_vers_la_fiche_rappel",
-    "date_de_publication",
-    "date_de_fin_de_la_procedure_de_rappel",
+    "id",
+    "name",
+    "coord",
+    "weather",
+    "main",
+    "visibility",
+    "wind",
+    "clouds",
+    "dt",
+    "sys",
 ]
 DB_FIELDS = COLUMNS_TO_KEEP + COLUMNS_TO_NORMALIZE + NEW_COLUMNS
